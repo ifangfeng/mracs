58f77794994ccdd8a24e6545832121715199d13f
Feng Fang <mathfangfeng@gmail.com>
2022-08-14 18:49:27 +0800

    fix

--- src/sigma_r.cpp
+++ src/sigma_r.cpp
@@ -7,9 +7,10 @@
 int main()
 {
     read_parameter();
-    auto g = read_in_Millennium_Run_galaxy_catalog(MilleCata);
+    force_kernel_type(1);
+    std::vector<Galaxy> g = read_in_Millennium_Run_galaxy_catalog(MilleCata);
     std::vector<Particle> p;
-    for(auto x : g) p.push_back({x.x, x.y, x.z, x.BulgeMass + x.StellarMass});
+    for(Galaxy x : g) p.push_back({x.x, x.y, x.z, x.BulgeMass + x.StellarMass});
 
     auto phi = read_in_phi(phiGenus);
     auto s = scaling_function_coefficients(phi, p);
@@ -17,15 +18,15 @@ int main()
 
     std::vector<double> r_log;
     std::vector<double> result;
-    const double rho_bar{p.size()*4./3*M_PI*pow(Radius*GridLen/SimBoxL, 3)};
+    const double rho_bar{p.size()*4./3*M_PI*pow(Radius/SimBoxL, 3)};
 
     for(int i = 0; i <= TESTPOINTS; ++i)
     {
         std::cout << "|" << std::setw(3) << i << "| th point: ";
         r_log.push_back(r0 * pow(r1/r0, i/static_cast<double>(TESTPOINTS)));
-        auto w = window_function_coefficients(phi, r_log[i]);
+        auto w = window_function_coefficients(phi, r_log[i], 0);
         auto c = inner_product_c2r(sc, w);
-        result.push_back(inner_product(c, c, GridNum)/pow(rho_bar, 2)/static_cast<double>(GridNum)-1);
+        result.push_back(inner_product(c, c, GridNum)/pow(rho_bar, 2)/-1);
     }
 
     for(auto x : r_log) std::cout << x << " "; std::cout << std::endl;
