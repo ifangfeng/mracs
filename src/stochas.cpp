b6afd1e58e87a01d8aa8e3000e6f94d938c5bff4
feng <mathfangfeng@gmail.com>
2022-11-15 23:15:43 +0000

    cp.cpp

--- src/stochas.cpp
+++ src/stochas.cpp
@@ -1,9 +1,23 @@
 #include"mracs.h"
 
-#define NUMRAN  1000
+#define NUMRAN  10000
 
 int main()
 {
+    std::string ofname_dm {"dm.txt"};
+    std::string ofname_halo {"halo.txt"};
+    std::string ofname_header {"header.txt"};
+
+    std::ofstream ofsm (ofname_dm);
+    std::ofstream ofsh (ofname_halo);
+    std::ofstream ofsd (ofname_header);
+
+    if(!ofsm || !ofsh || !ofsd)
+    {
+        std::cout << "Abort! opening write out file with error..." << std::endl;
+        std::terminate();
+    }
+
     read_parameter();
     auto p1 = read_in_TNG_3vector("/data0/BigMDPL/dm_particles_snap_079_position.bin");
     auto p20 = read_in_Halo_4vector("/data0/BigMDPL/BigMDPL_halo.bin");
@@ -19,6 +33,8 @@ int main()
     std::default_random_engine e;
     std::uniform_real_distribution<double> u(0, SimBoxL);
     std::vector<Particle> p0; for(size_t i = 0; i < NUMRAN; ++i) p0.push_back({u(e), u(e), u(e), 1.});
+
+    //for(size_t i = 0; i < 1e6; ++i) {p1.push_back({u(e), u(e), u(e), 1.}); p2.push_back({u(e), u(e), u(e), 1.});}
 
     double exp_m = p1.size() * 4./3 * M_PI * pow(Radius / SimBoxL,3);
     double exp_h = p2.size() * 4./3 * M_PI * pow(Radius / SimBoxL,3);
@@ -39,17 +55,23 @@ int main()
     delete[] c2;
     delete[] w;
 
+    std::cout << "hello1" << std::endl;
+
     for(size_t i = 0; i < NUMRAN; ++i){
         dtm[i] = dtm[i] / exp_m - 1;
         dth[i] = dth[i] / exp_h - 1;}
     
-    const int num_bin {10};
-    const double dtm0 {-1}, dtm1 {2};
+    const int num_bin {15};
+    const double dtm0 {-0.5}, dtm1 {0.5};
     const double ddt {(dtm1 - dtm0) / num_bin};
 
     std::vector<unsigned> count(num_bin);
-    std::vector<double> ave(num_bin), var(num_bin);
-    for(int i = 0; i < num_bin; ++i) {ave[i] = 0, var[i] = 0, count[i] = 0;}
+    std::vector<double> ave(num_bin), var(num_bin), cbin(num_bin);
+    for(int i = 0; i < num_bin; ++i) {ave[i] = 0; var[i] = 0; count[i] = 0;}
+    for(int i = 0; i < num_bin; ++i) {cbin[i] = dtm0 + (i + 0.5) * ddt;}
+
+
+    std::cout << "hello2" << std::endl;
 
     for(size_t i = 0; i < NUMRAN; ++i){
         int index = floor((dtm[i] - dtm0) / ddt);
@@ -59,7 +81,8 @@ int main()
             ++count[index];
         }
     }
-    for(size_t i = 0; i < NUMRAN; ++i){
+    std::cout << "hello3" << std::endl;
+    for(size_t i = 0; i < num_bin; ++i){
         if(count[i]) {
             ave[i] /= count[i];
             var[i] /= count[i];
@@ -67,34 +90,18 @@ int main()
         }
     }
 
-    std::cout << "delta_m bin: " << std::endl; for(int i = 0; i <= num_bin; ++i) std::cout << dtm0 + i*ddt << ", "; std::cout << std::endl;
     std::cout << "count in bin: " << std::endl; for(auto i : count) std::cout << i << ", "; std::cout << std::endl;
-    std::cout << "delta_h average: " << std::endl; for(auto i : ave) std::cout << i << ", "; std::cout << std::endl;
+    std::cout << "centre of bin: " << std::endl; for(auto i : cbin) std::cout << i << ", "; std::cout << std::endl;
+    std::cout << "delta_h: " << std::endl; for(auto i : ave) std::cout << i << ", "; std::cout << std::endl;
     std::cout << "delta_h deviation: " << std::endl; for(auto i : var) std::cout << sqrt(i) << ", "; std::cout << std::endl;
-
-    std::cout << "-----------------prj----------------" << std::endl << "===>dm: " << std::endl;
-    for(size_t i = 0; i < NUMRAN; ++i) std::cout << dtm[i] << ", "; std::cout << std::endl << "===>halo: " << std::endl;
-    for(size_t i = 0; i < NUMRAN; ++i) std::cout << dth[i] << ", "; std::cout << std::endl;
-
-
-
-
-
-    /*for(size_t i = 0; i < NUMRAN; ++i) 
-    {
-        if(prj_halo[i] < -exp_halo)
-            std::cout << "halo"<< p0[i].x << ",  "<< p0[i].y << ", "<< p0[i].z << ",  |"<< prj_halo[i]<< ", "<<std::endl;
-    };
-    for(size_t i = 0; i < NUMRAN; ++i) 
-    {
-        if(prj_dm[i] < -exp_dm)
-            std::cout << "dm" << p0[i].x << ",  "<< p0[i].y << ", "<< p0[i].z << ",  |"<< prj_dm[i]<< ", "<<std::endl;
-    };
-    */
-
-    //auto cic_dm = count_in_sphere(Radius,p1,p0);
-    //auto cic_halo = count_in_sphere(Radius,p1,p0);
-    //std::cout << "-----------------cic----------------" << std::endl << "===>dm: " << std::endl;
-    //for(size_t i = 0; i < NUMRAN; ++i) std::cout << cic_dm[i] / exp_dm - 1 << ", "; std::cout << std::endl << "===>halo: " << std::endl;
-    //for(size_t i = 0; i < NUMRAN; ++i) std::cout << cic_halo[i] / exp_halo - 1 << ", "; std::cout << std::endl;
+    std::cout << "writing text file...";
+    
+    for(size_t i = 0; i < NUMRAN; ++i) ofsm << dtm[i] << ", "; ofsm.close();
+    for(size_t i = 0; i < NUMRAN; ++i) ofsh << dth[i] << ", "; ofsh.close();
+    for(auto i : count) ofsd << i << ", "; ofsd << std::endl;
+    for(auto i : cbin) ofsd << i << ", "; ofsd << std::endl;
+    for(auto i : ave) ofsd << i << ", "; ofsd << std::endl;
+    for(auto i : var) ofsd << sqrt(i) << ", "; ofsd.close();
+    std::cout << std::endl;
+    
 }
