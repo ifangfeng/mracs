1cb3b45c593082824f928d415a988c092da9e982
feng <mathfangfeng@gmail.com>
2023-07-18 00:25:39 +0800

    .

--- src/rcmix.cpp
+++ src/rcmix.cpp
@@ -1,75 +1,69 @@
 // covariance of different halo bin 
 #include"mracs.h"
 
-double minimum_weight(std::vector<Particle>& v);
-double maximum_weight(std::vector<Particle>& v);
 
 int main(int argc, char** argv){
     read_parameter();
-
-    int nbin{0};
-    if(argc == 1) 
-        nbin = 1;
-    else {
-        nbin = std::stoi(argv[1]);
-        if(nbin < 1 || nbin > 128){
+    
+    int nbin{1};
+    if(argc>1){
+        int n = std::stoi(argv[1]);
+        if(n>=1 && n <= 128){
+            nbin = std::stoi(argv[1]);
+        }
+        else {
             std::cout << "input error, abort\n";
             return 0;
         }
     }
-    
+
     auto dm = read_in_DM_3vector("/data0/MDPL2/dm_sub/dm_sub5e-4.bin");
     auto hl = read_in_Halo_4vector("/data0/MDPL2/halo_Mcut2e12.bin");
+    auto hl_uni = read_in_Halo_3vector("/data0/MDPL2/halo_Mcut2e12.bin");
     std::string ifname {"output/envi_J10_GSR3_halo_Mcut2e12.txt"};
 
 
-    auto vpts = halo_envi_mass_multi_split(ifname, hl, nbin);
-    // auto vpts = halo_envi_mass_concatenate_split(ifname, hl, nbin);
+    auto envi_vpts = halo_envi_match_and_split(ifname, hl);
+    auto mass_vpts = halo_mass_split(hl, nbin);
+    auto con_vpts  = halo_envi_mass_concatenate_split(ifname, hl, nbin);
+    auto mul_vpts  = halo_envi_mass_multi_split(ifname, hl, nbin);
 
-    for(auto x : vpts) print_min_max_and_size(*x);
+    // -----mul_vpts check----
+    std::cout << "=======================\n";
+    for(auto x : mul_vpts) print_min_max_and_size(*x);
+    std::cout << "=======================\n";
+
 
     // ----reconstruct and check-------
+    auto sc_hl_uni = sfc_r2c(sfc(hl_uni),true);
     auto sc_hl = sfc_r2c(sfc(hl),true);
     auto sc_dm = sfc_r2c(sfc(dm),true);
 
-    auto sc_rc = optimal_reconstruct(dm,vpts,Radius,true);
+    auto sc_envi_rc = optimal_reconstruct(dm,envi_vpts,Radius,true);
+    auto sc_mass_rc = optimal_reconstruct(dm,mass_vpts,Radius,true);
+    auto sc_con_rc  = optimal_reconstruct(dm,con_vpts,Radius,true);
+    auto sc_mul_rc  = optimal_reconstruct(dm,mul_vpts,Radius,true);
     
     auto wpk = window_Pk(Radius,0);
 
-    auto cc1 = correlation_coefficients(sc_dm,sc_hl,wpk);
-    auto cc2 = correlation_coefficients(sc_dm,sc_rc,wpk);
+    auto cc_uni = correlation_coefficients(sc_dm,sc_hl_uni,wpk);
+    auto cc_hl  = correlation_coefficients(sc_dm,sc_hl,wpk);
+    auto cc_envi_rc = correlation_coefficients(sc_dm,sc_envi_rc,wpk);
+    auto cc_mass_rc = correlation_coefficients(sc_dm,sc_mass_rc,wpk);
+    auto cc_con_rc  = correlation_coefficients(sc_dm,sc_con_rc,wpk);
+    auto cc_mul_rc  = correlation_coefficients(sc_dm,sc_mul_rc,wpk);
     
     std::cout << "Cross-correlation coefficient:\n";
-    std::cout << "[mass picture]   default r_m: " << cc1 << "\n";
-    std::cout << "-----------RCST----------r_m: " << cc2 << "\n";
+    std::cout << "[number picture] default r_n: " << cc_uni << "\n";
+    std::cout << "[mass   picture] default r_m: " << cc_hl << "\n";
+    std::cout << "---RCST-------envi-------r_m: " << cc_envi_rc << "\n";
+    std::cout << "---RCST-------mass-------r_m: " << cc_mass_rc << "\n";
+    std::cout << "---RCST----envi + mass---r_m: " << cc_con_rc << "\n";
+    std::cout << "---RCST----envi * mass---r_m: " << cc_mul_rc << "\n";
     
 
 
     
 }
 
-
-//std::vector<std::vector<Particle>*> envi_and_mass_combine_split()
-
-
-
-double minimum_weight(std::vector<Particle>& v)
-{
-    double min{v[0].weight};
-    for(size_t i = 1; i < v.size(); ++i){
-        if(v[i].weight < min) min = v[i].weight;
-    }
-
-    return min;
-}
-
-double maximum_weight(std::vector<Particle>& v)
-{
-    double max{v[0].weight};
-    for(size_t i = 1; i < v.size(); ++i){
-        if(v[i].weight > max) max = v[i].weight;
-    }
-
-    return max;
-}
 
---
+++ src/rcpdf.cpp
@@ -0,0 +1,59 @@
+#include"mracs.h"
+
+int main(){
+    read_parameter();
+    auto dm = read_in_DM_3vector("/data0/MDPL2/dm_sub/dm_sub5e-4.bin");
+    auto hl = read_in_Halo_4vector("/data0/MDPL2/halo_Mcut2e12.bin");
+
+    // ----reconstructed hl----
+    auto vpts  = halo_mass_split(hl,32);
+    auto sc_rc = optimal_reconstruct(dm,vpts,Radius,true);
+
+    // ------convolve with window w------
+    auto w = wfc(Radius,0);
+    auto s_dm = convol3d(sfc(dm),w,true);
+    auto s_hl = convol3d(sfc(hl),w,true);
+    auto s_rc = convol_c2r(sc_rc, w);
+
+    // ----probability distribution function----
+    auto p0 = generate_random_particle(500,SimBoxL,0);
+
+    auto n_dm = project_value(s_dm,p0,true);
+    auto n_hl = project_value(s_hl,p0,true);
+    auto n_rc = project_value(s_rc,p0,true);
+
+
+}
+
+void prj_pdf(std::string ofn, double* c, std::vector<Particle>& p0, double min, double max, int nbin)
+{
+    std::ofstream ofs {ofn};
+    if(!ofs){
+        std::cout << "[pfj_pdf]: opening file " + ofn + " with error, abort!";
+        std::terminate();
+    }
+    const double delta {max - min};
+    const double norm {array_sum(c,GridVol) / GridVol};
+
+    auto n = project_value(c,p0,true);
+
+    #pragma omp parallel for
+    for(size_t i = 0; i < p0.size(); ++i) n[i] /= norm;
+
+    std::vector<size_t> count(nbin,0);
+
+    //#pragma omp parallel for reduction (+:count)
+    for(size_t i = 0; i < p0.size(); ++i) {
+        if(min <= n[i] && n[i] < max){
+            int index = (n[i] - min)/delta * nbin;
+            ++count[index];
+        }
+    }
+
+    for(int i = 0; i < nbin; ++i){
+        ofs << count[i] / delta * nbin / p0.size() << " ";
+    }
+
+    ofs << min << " " << max << " ";
+
+}
