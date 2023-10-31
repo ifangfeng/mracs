3a1bf64b349900d923a7a9b9a77e7f02ff5c5c87
feng <mathfangfeng@gmail.com>
2023-10-31 11:51:05 +0800

    .

--- src/MRACS_Split.cpp
+++ src/MRACS_Split.cpp
@@ -631,6 +631,31 @@ fftw_complex* optimal_reconstruct(fftw_complex* sc_dm, std::vector<std::vector<P
     return vec_sc[1];
 }
 
+void trimming_vpts(std::vector<std::vector<Particle>*>& vpts)
+{
+    // -------size check before eigen solver-----
+    bool EmptySize {false};
+    const int ThdSize {2};
+
+    for(auto x : vpts) 
+        if (x->size() < ThdSize) 
+            EmptySize = true;
+
+    if(EmptySize){
+        std::cout << "!EmptySize, some elements will be removed\n";
+        std::cout << "---+bf\n" << "size: ";
+        std::cout << vpts.size() << "\n";for(auto x : vpts) std::cout << x->size() << ", "; std::cout << "\n";
+        for(int i = 0; i < vpts.size(); ++i) {
+            if(vpts[i]->size() < ThdSize){
+                delete vpts[i];
+                vpts.erase(vpts.begin()+i);
+                --i;
+            }
+        }
+        std::cout << "---+af\n" << "size: ";
+        std::cout << vpts.size() << "\n";for(auto x : vpts) std::cout << x->size() << ", "; std::cout << "\n";
+    }
+}
 
 // vector of {sqrt(lambda),eigen_vector}
 std::vector<double> optimal_solution(std::vector<Particle>& dm, std::vector<std::vector<Particle>*>& vpts, double R, bool PRINT)
@@ -661,28 +686,3 @@ std::vector<double> optimal_solution_lean(fftw_complex* sc_dm, std::vector<std::
     return solve;
 }
 
-void trimming_vpts(std::vector<std::vector<Particle>*>& vpts)
-{
-    // -------size check before eigen solver-----
-    bool EmptySize {false};
-    const int ThdSize {2};
-
-    for(auto x : vpts) 
-        if (x->size() < ThdSize) 
-            EmptySize = true;
-
-    if(EmptySize){
-        std::cout << "!EmptySize, some elements will be removed\n";
-        std::cout << "---+bf\n" << "size: ";
-        std::cout << vpts.size() << "\n";for(auto x : vpts) std::cout << x->size() << ", "; std::cout << "\n";
-        for(int i = 0; i < vpts.size(); ++i) {
-            if(vpts[i]->size() < ThdSize){
-                delete vpts[i];
-                vpts.erase(vpts.begin()+i);
-                --i;
-            }
-        }
-        std::cout << "---+af\n" << "size: ";
-        std::cout << vpts.size() << "\n";for(auto x : vpts) std::cout << x->size() << ", "; std::cout << "\n";
-    }
-}
