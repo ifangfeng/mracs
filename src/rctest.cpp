c07934ec2251e3c315de575d0759e31cae1dc105
feng <mathfangfeng@gmail.com>
2023-11-06 13:09:16 +0800

    .

--- src/rctest.cpp
+++ src/rctest.cpp
@@ -1,97 +1,110 @@
-// noise cancelling
 #include"mracs.h"
 
 int main(){
     read_parameter();
 
-    const int Mbin{4};
-    double THR{30};
-    double GSR{1}; // Gaussian smoothing radius
-    double lth_opt_ME{17.5}; // optimal lambda_th
-    double lth_opt_NE{11.25};
+    auto dm = read_in_DM_3vector("/data0/MDPL2/dm_sub/dm_sub05.bin");
+    auto hl = read_in_Halo_4vector("/data0/MDPL2/halo_Mcut2e12.bin");
 
+    std::vector<Particle> hl_n; for(auto x : hl) hl_n.push_back({x.x,x.y,x.z,1.});
 
-    auto dm   = read_in_DM_3vector("/data0/MDPL2/dm_sub/dm_sub05.bin");
-    auto hl   = read_in_Halo_4vector("/data0/MDPL2/halo_Mcut2e12.bin");
-    auto hl_n = read_in_Halo_3vector("/data0/MDPL2/halo_Mcut2e12.bin");
+    std::ofstream ofs {"output/rcth-THR15-BS_complement.txt"};
+    std::string para{"JE="};
+
+    double GSR {1}; // Gaussian smoothing radius
+    double THR{15}; // Top-hat smoothing radius
 
     // ------environment sticker---------
     force_resoluton_J(10);
     force_base_type(0,1);
     force_kernel_type(2);
+    para+= std::to_string(Resolution);
 
     auto sc = sfc_r2c(sfc(dm),true);
     auto w_gs = wft(GSR, 0);
     auto cxx = tidal_tensor(sc, w_gs);
-    fftw_free(sc);
+    
+    int lMAX{20};
+    auto vec_lth = linear_scale_generator(0,lMAX,80,true);
+    std::vector<std::vector<int>> vec_env;
+    for(auto l_th : vec_lth) {
+        auto envi =  web_classify(cxx,hl,l_th);
+        vec_env.push_back(envi);
+    }
+
     delete[] w_gs;
-
-    auto env_ME = web_classify(cxx,hl,lth_opt_ME);
-    auto env_NE = web_classify(cxx,hl,lth_opt_NE);
-
+    fftw_free(sc);
     for(int i = 0; i < 6; ++i) delete[] cxx[i];delete[] cxx;
 
-    auto vpts_NE = halo_envi_match_and_split(env_NE,hl_n);
-    auto vpts_ME = halo_envi_mass_multi_split(env_ME, hl, Mbin);
-
-    // ----reconstruct-------
+    // ------cross-correlation of different Lambda_th---------
     force_resoluton_J(9);
     force_base_type(1,4);
     force_kernel_type(1);
-
-    auto wpk = window_Pk(THR,0);
-    auto sc_dm = sfc_r2c(sfc(dm),true);
-
-    trimming_vpts(vpts_NE);
-    trimming_vpts(vpts_ME);
+    para+=" , lMAX= " + std::to_string(lMAX) + " , JC="+std::to_string(Resolution);
+    para+=" , GSR= "+std::to_string(GSR) + " , THR= "+std::to_string(THR);
     
-    std::vector<fftw_complex*> vec_sc_NE, vec_sc_ME;
-    vec_sc_NE.push_back(sc_dm); for(auto x : vpts_NE) vec_sc_NE.push_back(sfc_r2c(sfc(*x),true));
-    vec_sc_ME.push_back(sc_dm); for(auto x : vpts_ME) vec_sc_ME.push_back(sfc_r2c(sfc(*x),true));
+    auto sc_dm = sfc_r2c(sfc(dm),true); std::vector<Particle>().swap(dm);
+    auto wpk = window_Pk(THR,0);
 
-    auto solve_NE = optimal_weight_solver(vec_sc_NE,wpk,true);
-    auto solve_ME = optimal_weight_solver(vec_sc_ME,wpk,true);
-
-    reconstruct_with_solve(vpts_NE,solve_NE);
-    reconstruct_with_solve(vpts_ME,solve_ME);
-
-    size_t npts_NE{0}, npts_ME{0};
-    for(auto x : vpts_NE) npts_NE += x->size();
-    for(auto x : vpts_ME) npts_ME += x->size();
-
-    auto ran_NE = default_random_particle(SimBoxL,npts_NE);
-    auto ran_ME = default_random_particle(SimBoxL,npts_ME);
-
-    // weight of vpts_NE to ran_NE
-    size_t n{0};
-    for(size_t i = 0; i < vpts_NE.size(); ++i){
-        for(size_t j = 0; j < vpts_NE[i]->size(); ++j){
-            ran_NE[n].weight = (*vpts_NE[i])[j].weight;
-            ++n;
+    std::vector<double> cc_mul_M, cc_mul_N, cc_M, cc_N;
+    // --- cc_mul_M ----
+    //const int LINE_M{3};
+    //for(size_t i = 0; i < LINE_M; ++i){
+    //    int Mbin = 1UL<<i; std::cout << "Mbin = " << Mbin << "\n";
+    //    for(auto env : vec_env){
+    //        auto vpts = halo_envi_mass_multi_split(env, hl, Mbin);
+    //        auto sol  = optimal_solution_lean(sc_dm,vpts,wpk,false);
+    //        cc_mul_M.push_back(sol[0]);
+    //        for(auto x : vpts) if(x->size()) std::vector<Particle>().swap(*x);
+    //    }
+    //}
+    // --- cc_E ---
+    const int LINE_N{3};
+    for(size_t i = 0; i < LINE_N; ++i){
+        int Nbin = 1UL<<i; std::cout << "Nbin = " << Nbin << "\n";
+        for(auto env : vec_env){
+            auto vpts = halo_envi_mass_multi_split(env, hl, Nbin);
+            for(auto x : vpts) for(auto &y : *x) y.weight = 1.; // make each particle of vpts equal to 1.
+            auto sol  = optimal_solution_lean(sc_dm,vpts,wpk,false);
+            cc_mul_N.push_back(sol[0]);
+            for(auto x : vpts) if(x->size()) std::vector<Particle>().swap(*x);
         }
     }
-    // weight of vpts_ME to ran_ME
-    n = 0;
-    for(size_t i = 0; i < vpts_ME.size(); ++i){
-        for(size_t j = 0; j < vpts_ME[i]->size(); ++j){
-            ran_ME[n].weight = (*vpts_ME[i])[j].weight;
-            ++n;
-        }
+    // --- cc_M ---
+    //const int num_Mbin{3};
+    //for(int i = 0; i < num_Mbin; ++i){
+    //    int Mbin = 1UL<<i; std::cout << "Mbin = " << Mbin << "\n";
+    //    auto vpts = halo_mass_split(hl,Mbin);
+    //    auto sol_M  = optimal_solution_lean(sc_dm,vpts,wpk,false);
+    //    cc_M.push_back(sol_M[0]);
+    //}
+    // --- cc_N ---
+    const int num_Nbin{3};
+    for(int i = 0; i < num_Nbin; ++i){
+        int Nbin = 1UL<<i; std::cout << "Nbin = " << Nbin << "\n";
+        auto vpts = halo_mass_split(hl,Nbin);
+        for(auto x : vpts) for(auto &y : *x) y.weight = 1.; // make each particle of vpts equal to 1.
+        auto sol_N  = optimal_solution_lean(sc_dm,vpts,wpk,false);
+        cc_N.push_back(sol_N[0]);
     }
-    auto pk_NE = densityPowerFFT(sfc_r2c(sfc(ran_NE),true));
-    auto pk_ME = densityPowerFFT(sfc_r2c(sfc(ran_ME),true));
 
-
-    std::vector<double*> vec_pk;
-
-    vec_pk.push_back(pk_NE);
-    vec_pk.push_back(pk_ME);
-
-
-    //--------------------------------
-    for(auto x : vec_pk) for(int i = 0; i < GridLen/2+1; ++i) std::cout << x[i] << ", ";std::cout << std::endl;
+    // ------------------------- output ----------------------------
+    ofs << para << '\n';
+    //ofs << "----->Mass weighted halo:  Mbin= "; for(int i = 0; i < LINE_M; ++i) ofs << (1UL<<i) << ", ";ofs << '\n'; 
+    //for(int i = 0; i < LINE_M; ++i){
+    //    for(int j = 0; j < vec_lth.size(); ++j)
+    //        ofs << cc_mul_M[i * vec_lth.size() + j] << ", ";
+    //    ofs << std::endl;
+    //}
+    ofs << "----->Equal weighted halo: Nbin= "; for(int i = 0; i < LINE_N; ++i) ofs << (1UL<<i) << ", ";ofs << '\n'; 
+    for(int i = 0; i < LINE_N; ++i){
+        for(int j = 0; j < vec_lth.size(); ++j)
+            ofs << cc_mul_N[i * vec_lth.size() + j] << ", ";
+        ofs << std::endl;
+    }
+    //ofs << "----->Mass split, Mass weighted:  Mbin= "; for(int i = 0; i < num_Mbin; ++i) ofs << (1UL<<i) << ", ";ofs << '\n'; 
+    //for(auto x : cc_M) ofs << x << ", "; ofs << std::endl;
+    ofs << "----->Mass split, Equal weighted: Nbin= "; for(int i = 0; i < num_Nbin; ++i) ofs << (1UL<<i) << ", ";ofs << '\n'; 
+    for(auto x : cc_N) ofs << x << ", "; ofs << std::endl;
 
 }
-
-
-
